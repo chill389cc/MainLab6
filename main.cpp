de0dd46cd65f2bc2720350fd840526f6b126df0d
Caleb Hill <chill389cc@gmail.com>
2021-10-13 11:11:11 -0600

    Adding a quick description of what we need to do
    Although most of the details will need to be read off of the zybooks website

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 
 /*
  * Main Lab 6 - Hot plate
+ * https://learn.zybooks.com/zybook/BYUCS142Fall2021/chapter/6/section/33
  * Pair programming:
  *  Caleb Hill - Section 002
  *  Michael    - Section ###
- *
+ */
+/*  Basic Requirements:
+ *      Must be done using pair programming:
+ *          * In the Canvas submission quiz for the lab, state if you did pair programming, who your partner was, and whether you used proper pair programming.
+ *          * Submit the final code using zyBooks from both of your accounts. Your submitted code should be exactly the same.
+ *          * For your safety, if meeting up in person, meet in the TMCB labs or some other public place to work.
+ *          * Worked "side-by-side" on ALL aspects of creating the program, including the design, the implementation and the testing of the code.
+ *          * Evenly split coding time as "driver" and "navigator", and switched roles frequently.
+ *          * The navigator actively reviewed each line of code as it was typed in, watched for errors, style issues, etc. and gave input to the driver, while also considering strategic direction for upcoming lines of code, etc.
+ *          * You took time to teach each other, and both of you understand all aspects of your submitted program.
+ *      Part 1 - Initialize and Print 2D Array
+ *      Part 2 - Update Elements Once
+ *      Part 3 - Repeat Update until Steady State is Achieved
+ *      Part 4 - Write Data to a File
+ *      Part 5 - Input Plate
+ *      Part 6 - Use Excel to Graph the Results
  */
 
 int main() {
