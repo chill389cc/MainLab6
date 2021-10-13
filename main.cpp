dbf369d9036d236646723e601ad575aa219d21ad
Caleb Hill <chill389cc@gmail.com>
2021-10-13 10:57:57 -0600

    Initial repo creation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+/*
+ * Main Lab 6 - Hot plate
+ * Pair programming:
+ *  Caleb Hill - Section 002
+ *  Michael    - Section ###
+ *
+ */
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
