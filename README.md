23b6efa5462dedc9f29fea80c0582a30dbdc8000
Caleb Hill <chill389cc@gmail.com>
2021-10-13 10:56:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MainLab6
+ Shared Repo for Main Lab 6 - Hot Plate
---
+++ cmake-build-debug/CMakeFiles/MainLab6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainLab6.dir/main.cpp.o"
+  "MainLab6"
+  "MainLab6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainLab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
